798bfa2044c3a4679ddadb87b0d30e817b875904
alidemirkaya <ademirkaya@industryolog.com>
2017-06-30 12:25:23 +0300

    Add files via upload

---
+++ Grafik Soruları-16-50-100/16.m
@@ -0,0 +1,12 @@
+tahil_uretimi = [31 36 18 8 7];
+h = pie(tahil_uretimi);
+
+%Baþlýk Eklemek pie komutundan sonra aþaðýdaki þekilde kullanýyoruz
+
+tahil_uretimi = [31 36 18 8 7];
+pie(tahil_uretimi,{'Malatya','Konya','Ankara','Ýzmir','Ýstanbul'})
+
+%Üç boyutlu grafik oluþturmak için pie3 fonksiyonu kullanýlýr
+
+tahil_uretimi = [31 36 18 8 7];
+pie3(tahil_uretimi,{'Malatya','Konya','Ankara','Ýzmir','Ýstanbul'})
