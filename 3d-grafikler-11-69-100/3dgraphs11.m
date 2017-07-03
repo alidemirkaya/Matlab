192ff013ecb075536f2b6fc59ca403f5c158c17f
alidemirkaya <ademirkaya@industryolog.com>
2017-07-03 03:17:04 +0300

    Add files via upload

--- 3d-grafikler-11-69-100/3dgraphs11.m
+++ 3d-grafikler-11-69-100/3dgraphs11.m
@@ -1,8 +1,8 @@
-[x,y,z] = peaks;
-% Create black dashed contours
-[C,h] = contour(x,y,z,'--k');
-[text_handles] = clabel(C,h);
-% Modify the labels to make them bigger and blue.
-set(text_handles,'fontsize',15,'color','blue'); 
-contourf(x,y,z,[-10:10],'--k');
-colorbar
+clear all;
+clc
+[X Y]=meshgrid(-3:3,-3:3);
+R=sqrt(X.^2+Y.^2);
+Z=cos(R)./R;
+c=contour(X,Y,Z,3);%3 Tane çizgi çiziyoruz
+clabel(c)
+title('Contour Komutu Ýle Çizim')
